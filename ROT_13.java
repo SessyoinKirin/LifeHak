cd1e5ed6f1e2b2f3cc48060fdb486f08cd704df8
Chen <neosdiva@gmail.com>
2021-06-25 19:52:38 -0300

    criptografia ROT-13

---
+++ ROT_13.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ROT_13 {
+	
+	public static void main (String[] args) {
+		
+		Scanner ler = new Scanner(System.in);
+		String texto = "", cara = "abcdefghijklmnopqrstuvwxyz", coroa = "nopqrstuvwxyzabcdefghijklm", saida="";
+		boolean encontrou = false;
+		System.out.print("Entre com um texto: ");
+		texto = ler.nextLine();
+		
+		for(int i =0; i<texto.length(); i++){
+			encontrou = false;
+			for(int j = 0; ! encontrou && j<cara.length(); j++){
+				if(texto.charAt(i) == cara.charAt(j)){
+					encontrou=true;
+					saida+=coroa.charAt(j);
+					}
+				}
+				
+			if(! encontrou){
+				saida+=texto.charAt(i);
+				}
+			}
+			System.out.println("Saida: "+saida);
+	}
+}
+
